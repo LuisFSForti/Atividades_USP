a44c4bc8855e78482abaa5a6c439d929869e651f
LuisFSForti <luisforti@usp.br>
2025-10-01 20:28:31 -0300

    Finalização do projeto de computação gráfica

---
+++ Computacao_Grafica/Trabalho1/Trabalho1Qt/Build/Windows/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_Gui_0_FILE C:/Users/Luis/Documents/Facul/Atividades_USP/Computacao_Grafica/Trabalho1/Trabalho1Qt/Build/Windows/Gui_0.exe)
+set(__QT_DEPLOY_TARGET_Gui_0_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_Gui_0_RUNTIME_DLLS C:/Qt/6.9.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.2/mingw_64/bin/Qt6Core.dll)
