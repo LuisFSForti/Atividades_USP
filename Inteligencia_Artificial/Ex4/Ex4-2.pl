0ec7923dc05d390deac2ddd72738754c527f25bd
LuisFSForti <luisforti@usp.br>
2026-03-26 22:42:04 -0300

    Correção do ex 4 de IA

--- Inteligencia_Artificial/Ex4/Ex4-2.pl
+++ Inteligencia_Artificial/Ex4/Ex4-2.pl
@@ -2,8 +2,10 @@
 
 % Para incluir o cálculo de reprovação
 :- consult('Ex4-1.pl').
+% Para incluir o aluno "Carlos Silva" do arquivo anterior
+:- multifile aluno/11.
 
-aluno("Carlos Silva", "Rua das Orquideas, 32", "016966666666", "05/05/1955", 10,10,10,10,4,8,100).
+aluno("Carlos Forti", "Rua das Orquideas, 32", "016966666666", "05/05/1955", 10,10,10,10,4,8,100).
 aluno("Mari Pereira", "Rua das Orquideas, 32", "016966666666", "05/05/1955", 10,2,10,3,4,8,30).
 aluno("Joao Barrosa", "Rua das Orquideas, 32", "016966666666", "05/05/1955", 6,10,7,10,4,8,60).
 aluno("Matias Luzes", "Rua das Orquideas, 32", "016966666666", "05/05/1955", 0,0,0,0,0,0,0).
@@ -12,14 +14,14 @@ aluno("Matias Luzes", "Rua das Orquideas, 32", "016966666666", "05/05/1955", 0,0
 % a)
 
 ?- aluno(Nome, _, _, _, _, 10, _, 10, _, _, _).
-Nome = "Carlos Silva" ;
+Nome = "Carlos Forti" ;
 Nome = "Joao Barrosa".
 
 % b)
 
 ?- aluno(Nome, _, _, _, 10, _, 10, _, _, _, _).
-Nome = "Carlos Silva" ;
-Nome = "Mari Pereira" .
+Nome = "Carlos Forti" ;
+Nome = "Mari Pereira".
 
 % c)
 
@@ -43,18 +45,15 @@ Freq = 0.
 
 % e)
 
-?- situacao_sem_rec(Nome, Situacao), Situacao = reprovado.
-Nome = "Matias Luzes",
-Situacao = reprovado.
+?- situacao_sem_rec(Nome, reprovado).
+Nome = "Matias Luzes".
 
 % f)
 
-?- situacao_sem_rec(Nome, Situacao), Situacao = aprovado.
-Nome = "Carlos Silva",
-Situacao = aprovado ;
-Nome = "Mari Pereira",
-Situacao = aprovado ;
-Nome = "Joao Barrosa",
-Situacao = aprovado .
+?- situacao_sem_rec(Nome, aprovado).
+Nome = "Carlos Silva" ;
+Nome = "Carlos Forti" ;
+Nome = "Mari Pereira" ;
+Nome = "Joao Barrosa" .
 
 */
